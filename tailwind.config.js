57ebb686f7ba0e5dd1df65edfe5f256c43474975
James <james.otoole@ubc.ca>
2022-09-29 15:17:35 -0700

    Adjust text sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,12 +59,13 @@ module.exports = {
       xs: ".75rem", // 12px
       sm: ".875rem", // 14px
       base: "1rem", // 16px
-      lg: "1.25rem", // 18px
-      xl: "1.5rem", // 24px
-      "2xl": "1.75rem", // 28px
-      "3xl": "2rem", // 32px
-      "4xl": "3rem", // 48px
-      "5xl": "4rem", // 64px
+      lg: "1.125rem", // 18px
+      xl: "1.25rem", // 20px
+      "2xl": "1.5rem", // 24px
+      "3xl": "1.75rem", // 28px
+      "4xl": "2rem", // 32px
+      "5xl": "3rem", // 48px
+      "6xl": "4rem", // 64px
     },
     extend: {
       margin: {
