ec4e90bca57cf65038cee90398ffefc2d779a7b7
James O'Toole <james.otoole@ubc.ca>
2024-03-22 14:23:37 -0700

    Add prefixed styles for ckeditor5

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,11 @@ module.exports = {
     require("postcss-import"),
     require("postcss-simple-vars"),
     require("tailwindcss"),
+    require("postcss-prefix-selector")({
+      prefix: ".ck-editor",
+      exclude: ["body", "html", ":root"],
+      includeFiles: "src/css/drupal.ckeditor.theme.styles.css",
+    }),
     require("autoprefixer"),
     ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : [])
   ]
